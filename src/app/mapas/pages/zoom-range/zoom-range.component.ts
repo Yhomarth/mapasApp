3df00c024f2aea16ff0efad8f5d4a6cad9fac4c7
Yhomarth Reyes <yhomarth@gmail.com>
2022-05-07 08:24:27 -0400

    Página Zoom-Range - Diseño

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,9 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
   styles: [
+
+    `
+    .mapa-container{
+    width: 100%;
+    height: 100%;
+    }
+
+    .row{
+      background-color: white;
+      border-radius: 5px;
+      bottom: 50px;
+      left: 50px;
+      padding: 10px;
+      position: fixed;
+      z-index: 999;
+    }
+    `
+
+
   ]
 })
 export class ZoomRangeComponent implements OnInit {
@@ -11,6 +31,16 @@ export class ZoomRangeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+
+    const map = new mapboxgl.Map({
+      container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-69.84037583114966, 18.48835890524846],
+      zoom: 18
+    });
+
+
   }
 
 }
