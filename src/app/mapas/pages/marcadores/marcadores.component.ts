27f19a31bbce82fd57a427bdc03935f40662cad7
Yhomarth Reyes <yhomarth@gmail.com>
2022-05-25 18:05:41 -0400

    Agregada la modalidad de volar hacia los marcadores

--- src/app/mapas/pages/marcadores/marcadores.component.ts
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -1,6 +1,11 @@
 import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 
+interface MarcadorColor {
+  color : string,
+  marker : mapboxgl.Marker
+}
+
 @Component({
   selector: 'app-marcadores',
   templateUrl: './marcadores.component.html',
@@ -12,6 +17,17 @@ import * as mapboxgl from 'mapbox-gl';
     height: 100%;
     }
 
+    .list-group {
+      position : fixed;
+      top: 20px;
+      right: 20px;
+      z-index: 99; 
+    }
+
+    li{
+      cursor : pointer;
+    }
+
     `
   ]
 })
@@ -21,6 +37,7 @@ export class MarcadoresComponent implements AfterViewInit {
   @ViewChild('mapa') divMapa !: ElementRef;
   zoomLevel : number = 16;
   center: [number, number] = [-69.84037583114966, 18.48835890524846];
+  marcadores : MarcadorColor[] = [];
 
   constructor() { }
 
@@ -36,9 +53,42 @@ export class MarcadoresComponent implements AfterViewInit {
     // const markerHtml : HTMLElement = document.createElement('div');
     // markerHtml.innerHTML = 'Hola mundo';
 
-    new mapboxgl.Marker()
+    // new mapboxgl.Marker()
+    //     .setLngLat(this.center)
+    //     .addTo(this.mapa);
+
+
+
+    }
+
+    irMarcador(marcador: mapboxgl.Marker){
+      console.log(marcador.getLngLat()  );
+
+      const {lng, lat} = marcador.getLngLat();
+      this.mapa.flyTo({
+        center  : [ lng, lat  ]
+      });
+
+    }
+
+    agregarMarcador(){
+      const color = "#xxxxxx".replace(/x/g, y=>(Math.random()*16|0).toString(16));
+
+      const nuevoMarcador = new mapboxgl.Marker(
+        {
+          draggable: true,
+          color
+        }
+      )
         .setLngLat(this.center)
         .addTo(this.mapa);
+
+        this.marcadores.push({
+          color,
+          marker : nuevoMarcador
+        });
+
+      
 
     }
 
